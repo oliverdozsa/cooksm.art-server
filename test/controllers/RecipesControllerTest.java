aebbb36d99be3b70890d754e7c2b1174fd51b743
Oliver Dozsa <dozsa.oliver@gmail.com>
2019-11-03 15:56:18 +0100

    Renaming tests.

--- test/controllers/RecipesControllerTest.java
+++ test/controllers/RecipesControllerTest.java
@@ -34,9 +34,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOf_ExcludedOverlaps() {
+    public void testGetByIngredients_ComposedOf_ExcludedOverlaps() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOf_ExcludedOverlaps");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOf_ExcludedOverlaps");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&limit=50&unknownIngs=4&unknownIngsRel=le&goodIngs=2&goodIngsRel=ge&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=5&inIngs[0]=1&inIngs[1]=2&exIngs[0]=4&exIngs[1]=5";
@@ -54,9 +54,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_IncludedExcludedNotMutEx() {
+    public void testGetByIngredients_IncludedExcludedNotMutEx() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_IncludedExcludedNotMutEx");
+        logger.info("-- RUNNING TEST: testGetByIngredients_IncludedExcludedNotMutEx");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=4&unknownIngsRel=le&goodIngs=2&goodIngsRel=ge&limit=50&unknownIngs=4&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=5&inIngs[0]=1&inIngs[1]=2&exIngs[0]=4&exIngs[1]=2";
@@ -69,9 +69,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOf_Commons() {
+    public void testGetByIngredients_ComposedOf_Commons() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOf_Commons");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOf_Commons");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=4&unknownIngsRel=le&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&&minIngs=1&maxIngs=5&inIngs[0]=1&inIngs[1]=3&exIngs[0]=5";
@@ -90,9 +90,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOf_CommonsWithTags() {
+    public void testGetByIngredients_ComposedOf_CommonsWithTags() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOf_CommonsWithTags");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOf_CommonsWithTags");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=4&unknownIngsRel=le&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=5&inIngs[0]=1&inIngs[1]=3&exIngTags[0]=6";
@@ -111,9 +111,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfStrict() {
+    public void testGetByIngredients_ComposedOfStrict() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfStrict");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfStrict");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=3&goodIngsRatio=1.0&limit=50&offset=0&orderBy=name&orderBySort=asc&isAdditiveIngs=true&minIngs=1&maxIngs=4&inIngs[0]=1&inIngs[1]=2&inIngs[2]=3&inIngs[3]=4";
@@ -131,9 +131,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfStrictTagsOnly() {
+    public void testGetByIngredients_ComposedOfStrictTagsOnly() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfStrictTags");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfStrictTagsOnly");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=3&goodIngsRatio=1.0&limit=50&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=4&inIngs[0]=10&inIngTags[0]=1&inIngTags[1]=2";
@@ -151,9 +151,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfAnyOf() {
+    public void testGetByIngredients_ComposedOfAnyOf() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfAnyOf");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfAnyOf");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=0&goodIngsRel=gt&limit=50&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=4&inIngs[0]=4";
@@ -171,9 +171,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfAnyOfTagsOnly() {
+    public void testGetByIngredients_ComposedOfAnyOfTagsOnly() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfAnyOfTagsOnly");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfAnyOfTagsOnly");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=0&goodIngsRel=gt&limit=50&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=8&inIngTags[0]=5";
@@ -191,9 +191,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfAnyOfTagsAndIngrs() {
+    public void testGetByIngredients_ComposedOfAnyOfTagsAndIngrs() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfAnyOfTagsAndIngrs");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfAnyOfTagsAndIngrs");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=0&goodIngsRel=gt&limit=50&offset=0&orderBy=name&orderBySort=asc&minIngs=1&maxIngs=8&inIngs[0]=4&inIngTags[0]=1";
@@ -211,9 +211,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfExact() {
+    public void testGetByIngredients_ComposedOfExact() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfExact");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfExact");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=le&goodIngs=3&goodIngsRel=eq&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=5&inIngs[1]=6&inIngs[2]=7";
@@ -231,9 +231,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfAtLeast() {
+    public void testGetByIngredients_ComposedOfAtLeast() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfAtLeast");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfAtLeast");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=5&inIngs[1]=6&exIngs[0]=4";
@@ -251,9 +251,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_ComposedOfAtLeastWithExTags() {
+    public void testGetByIngredients_ComposedOfAtLeastWithExTags() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_ComposedOfAtLeastWithExTags");
+        logger.info("-- RUNNING TEST: testGetByIngredients_ComposedOfAtLeastWithExTags");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=5&inIngs[1]=6&exIngs[0]=4&exIngTags[0]=3";
@@ -270,9 +270,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_FilterbyNames() {
+    public void testGetByIngredients_FilterbyNames() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_FilterbyNames");
+        logger.info("-- RUNNING TEST: testGetByIngredients_FilterbyNames");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=5&inIngs[1]=6&nameLike=e_3";
@@ -290,9 +290,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesByIngredients_FilterbySourcePages() {
+    public void testGetByIngredients_FilterbySourcePages() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesByIngredients_FilterbySourcePages");
+        logger.info("-- RUNNING TEST: testGetByIngredients_FilterbySourcePages");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "searchMode=1&unknownIngs=0&unknownIngsRel=ge&goodIngs=2&goodIngsRel=ge&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=5&inIngs[1]=6&sourcePages[0]=4";
@@ -310,9 +310,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesAll_WithExIngTagsOnly() {
+    public void testGetAll_WithExIngTagsOnly() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesAll_WithExIngTagsOnly");
+        logger.info("-- RUNNING TEST: testGetAll_WithExIngTagsOnly");
         logger.info("------------------------------------------------------------------------------------------------");
 
         Http.RequestBuilder httpRequest = new Http.RequestBuilder().method(GET).uri(
@@ -328,9 +328,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesAll() {
+    public void testGetAll() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesAll");
+        logger.info("-- RUNNING TEST: testGetAll");
         logger.info("------------------------------------------------------------------------------------------------");
 
         Http.RequestBuilder httpRequest = new Http.RequestBuilder().method(GET).uri(
@@ -346,9 +346,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetSingleRecipe() {
+    public void testGetSingle() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetSingleRecipe");
+        logger.info("-- RUNNING TEST: testGetSingle");
         logger.info("------------------------------------------------------------------------------------------------");
 
         Http.RequestBuilder httpRequest = new Http.RequestBuilder().method(GET).uri(routes.RecipesController.singleRecipe(3, 0).url());
@@ -362,9 +362,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetSingleRecipe_NotFound() {
+    public void testGetSingle_NotFound() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetSingleRecipe_NotFound");
+        logger.info("-- RUNNING TEST: testGetSingle_NotFound");
         logger.info("------------------------------------------------------------------------------------------------");
 
         Http.RequestBuilder httpRequest = new Http.RequestBuilder().method(GET).uri(routes.RecipesController.singleRecipe(-4, 0).url());
@@ -457,9 +457,9 @@ public class RecipesControllerTest {
 
     @Test
     @DataSet(value = "datasets/yml/recipes.yml", disableConstraints = true, cleanBefore = true)
-    public void testGetRecipesRatio() {
+    public void testGetByRatio() {
         logger.info("------------------------------------------------------------------------------------------------");
-        logger.info("-- RUNNING TEST: testGetRecipesRatio");
+        logger.info("-- RUNNING TEST: testGetByRatio");
         logger.info("------------------------------------------------------------------------------------------------");
 
         String reqParams = "?searchMode=3&goodIngsRatio=0.6&limit=50&offset=0&orderBy=name&orderBySort=asc&inIngs[0]=2&inIngs[1]=3";
