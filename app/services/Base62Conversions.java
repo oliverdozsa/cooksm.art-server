799caa05d32ddf33e06fc3a23c3abb961cd31c3b
Oliver Dozsa <dozsa.oliver@gmail.com>
2021-12-04 08:43:20 +0100

    Refactored base62 conversions.

--- app/services/Base62Conversions.java
+++ app/services/Base62Conversions.java
@@ -2,22 +2,27 @@ package services;
 
 import io.seruco.encoding.base62.Base62;
 
-import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
 class Base62Conversions {
     private static Base62 base62 = Base62.createInstance();
 
     public static String encode(Long value) {
-        BigInteger bigInt = BigInteger.valueOf(value);
-        byte[] bigIntBytes = bigInt.toByteArray();
-        byte[] encodedBytes = base62.encode(bigIntBytes);
+        ByteBuffer longBytes = ByteBuffer.allocate(Long.BYTES);
+        longBytes.putLong(value);
+
+        byte[] encodedBytes = base62.encode(longBytes.array());
         return new String(encodedBytes);
     }
 
     public static Long decode(String value) {
         byte[] encodedBytes = value.getBytes();
         byte[] decodedBytes = base62.decode(encodedBytes);
-        BigInteger bigInt = new BigInteger(decodedBytes);
-        return bigInt.longValue();
+
+        ByteBuffer longBytes = ByteBuffer.allocate(Long.BYTES);
+        longBytes.put(decodedBytes);
+        longBytes.flip();
+
+        return longBytes.getLong();
     }
 }
